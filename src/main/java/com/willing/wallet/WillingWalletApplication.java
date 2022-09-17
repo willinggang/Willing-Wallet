bbce1b8da6e16142e914abbb1eb133345c7f8f8d
sunzhigang <a864307818@qq.com>
2022-09-17 22:54:00 +0800

    Initial commit

---
+++ src/main/java/com/willing/wallet/WillingWalletApplication.java
@@ -0,0 +1,13 @@
+package com.willing.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillingWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WillingWalletApplication.class, args);
+    }
+
+}
